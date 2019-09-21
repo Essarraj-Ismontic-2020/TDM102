31ff1c435c61efcc4ec6f1dea5aaa7f6d5cf8daf
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-09-21 16:55:34 +0100

    save

---
+++ Programmation structurée/P5 - Type de base, Langage Groupe 2/A52 - Présentations/Yassine El atlassi/int/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int number = 3;
+printf("nuber is: %d",number);
+	return 0;
+}
