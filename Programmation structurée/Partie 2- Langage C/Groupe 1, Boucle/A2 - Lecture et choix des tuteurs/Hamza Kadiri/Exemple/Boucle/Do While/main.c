48f2281d1320616a92f7958cac857c15746e5bb1
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-03 15:08:39 +0100

    save

---
+++ Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A2 - Lecture et choix des tuteurs/Hamza Kadiri/Exemple/Boucle/Do While/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+    int i = 0;
+
+    do
+    {
+        printf("La variable i vaut %d\n", i);
+        ++i;
+    } while (i < 5);
+
+    return 0;
+}
---
+++ Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A2 - Lecture et choix des tuteurs/Hamza Kadiri/Exemple/Boucle/For/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+for (int i = 0; i < 5; ++i)
+        printf("la variable i vaut %d\n", i);
+
+    return 0;
+}
