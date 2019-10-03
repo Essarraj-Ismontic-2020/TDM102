d790f1e608b792ba8632a565e4a2ab634266b4e6
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-03 14:04:55 +0100

    SAVE

---
+++ Programmation structurée/Partie 2- Langage C/Groupe A/Zaoujal Omar TP/booléen/main4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int age ;
+	printf("how old are you ? \n");
+	scanf("your age \n");
+	if (age>=18){
+		printf("YES");
+	}
+	else {
+		printf("NO");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
