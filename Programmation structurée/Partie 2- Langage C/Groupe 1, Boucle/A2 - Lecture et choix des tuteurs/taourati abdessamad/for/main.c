9b73e3bdcadc650e731d44dfefe469c4e95875e4
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-04 11:35:40 +0100

    Save kadiri

---
+++ Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A2 - Lecture et choix des tuteurs/taourati abdessamad/for/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int compteur;
+
+for (compteur = 0 ; compteur < 10 ; compteur++)
+{
+    printf("Salut les Zeros !\n");
+}
+	return 0;
+}
