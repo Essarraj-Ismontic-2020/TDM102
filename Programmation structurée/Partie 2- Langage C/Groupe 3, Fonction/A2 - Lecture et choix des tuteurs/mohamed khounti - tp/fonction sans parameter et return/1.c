1f5bc46a6b70eead43f5b2ae0498ed6345dcad61
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-07 09:59:39 +0100

    khounti--  correct de example simple

--- Programmation structurée/Partie 2- Langage C/Groupe 3, Fonction/A2 - Lecture et choix des tuteurs/mohamed khounti - tp/fonction sans parameter et return/1.c
+++ Programmation structurée/Partie 2- Langage C/Groupe 3, Fonction/A2 - Lecture et choix des tuteurs/mohamed khounti - tp/fonction sans parameter et return/1.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 
-rgbgggfsdfhkhhkkkuujtjtttj
-void somme(int nombre1, int nombre2){
+
+void somme(){
 	int somme ;
+	int nombre1 = 20 ; int nombre2 = 20;
 	somme = nombre1  + nombre2;
 	printf("somme est  %d\n",somme);	
 }
 
 int main(int argc, char *argv[]) {		
-somme(10,10);
-somme(20,10);
-somme(30,10);		
+somme();
+		
 return 0;
 }
 
