0b823ae99f8f31658ed4bd409bc6dd321ebf5a7c
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-10 14:52:05 +0100

    save mouad kebyani

---
+++ Programmation structurée/Partie 2- Langage C/Groupe E/fonction/Anouar/Avec return/main78.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int somme(int a, int b){
+	return a+b;
+}
+
+int main(){
+	int a=10;
+	int b=5;
+	printf("La somme est %d",somme(a,b));
+}                                                                                                                                                                    
---
+++ Programmation structurée/Partie 2- Langage C/Groupe E/fonction/Fatima Zahrae/avec return/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int somme (int a, int b){
+	return a+b;
+	
+}
+
+int main(int argc, char *argv[]) {
+	int f;
+	f=somme (10,20);
+	printf ("résulta est %d",f);
+	return 0;
+}
+
+
+
+
+
+
