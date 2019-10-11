478c8b2ebd50fd4ed628d653387765e3288c1e66
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-11 12:31:57 +0100

    save

---
+++ Programmation structurée/Partie 2- Langage C/Groupe 2, Expression/A2 - Lecture et choix des tuteurs/EXAMPLE COMPOSé/Zaoujal Omar/ex composé/expression booléen composé avec boucles while/mainexpbooléencomposéavecwhile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	int a = 2 ;
+	while (a<10){
+	printf("hello %d\n",a);
+			(a++);
+	
+	}
+	if (a<2||a>2){
+		printf("false");
+			
+
+	}
+	else {
+		printf("true");
+
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
