a620a773d98879ced6cc3f7f1200687dc76cbc53
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-14 09:13:43 +0100

    SAVE kEBYANI

--- Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A3-Exemple composé/Mouad kebyani/1.Boucle avec continue/main.c
+++ Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A3-Exemple composé/Mouad kebyani/1.Boucle avec continue/main.c
@@ -4,16 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a=1;
-	int b=1;
-	while(a<=3) {
-		printf("Apprenant numero %d \n",a);
-		a++;
-
-	} 
-	while(b<=2) {
-		printf("rapporteur numero %d \n",b);
-		b++;
-	} 
+	int nombre=0;
+	while (nombre<10)
+	{nombre++;
+	if (nombre==5)
+	{continue;}
+	printf("la valeur est %d \n",nombre);
+	}
 	return 0;
 }
---
+++ Programmation structurée/Partie 2- Langage C/Groupe 1, Boucle/A3-Exemple composé/Mouad kebyani/6.Afficher les valeurs positif d'un tableau d'entier/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tableau[4]={18,-2,-35,956};
+    int i;
+    for(i=0;i<4;i++)
+    { 
+    if (tableau[i]>0)
+       printf("%d\n",tableau[i]);
+    }
+
+    return 0;
+}
+
