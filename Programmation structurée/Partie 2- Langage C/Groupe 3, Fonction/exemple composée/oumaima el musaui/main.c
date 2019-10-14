e35d55c5a55db45b53ef18fbff7785bbf11b136c
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-14 09:30:24 +0100

    save el musaui

---
+++ Programmation structurée/Partie 2- Langage C/Groupe 3, Fonction/exemple composée/oumaima el musaui/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+float addition (float a ,float b){
+	float soulution ;
+	soulution = a+b;
+	return soulution;
+}
+float soustraction(float a , float b){
+	float soulution ;
+	soulution = a-b;
+	return soulution;
+}
+float devision (float a, float b){
+	float soulution;
+	soulution = a*b;
+	return soulution ;
+}
+float multiplication( float a, float b ){
+	float soulution ;
+	soulution = a/b;
+	return soulution;
+}
+float calculatrice ( float a , float b,char op){
+	float soulution ;
+
+	switch (op)
+	{
+	
+		case '+' :
+			soulution = addition ( a,b);
+			break;
+			case '-' :
+				soulution = soustraction (a,b);
+				break;
+				case '*':
+					soulution = devision (a,b);
+					break;
+					case '/' :
+						soulution = multiplication (a,b);
+						break ;
+						
+			
+	}
+	return soulution ;
+}
+
+int main(int argc, char *argv[]) {
+	char op ;
+	printf ("donner votre op ");
+	scanf ("%c",&op);
+	
+	
+	float a;
+	printf("donnez un nombre");
+	scanf ("%f",&a);
+
+	float b;
+	printf("donnez un nombre");
+	scanf("%f",&b);
+
+	float soulution = calculatrice ( a , b , op);
+	printf ("%f",soulution);
+	return 0;
+}
