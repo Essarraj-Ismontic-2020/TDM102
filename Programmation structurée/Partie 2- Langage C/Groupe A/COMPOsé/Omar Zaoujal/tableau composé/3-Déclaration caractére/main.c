3043fba0fe2cb0ec758a01bfc76c102f3f2b6673
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-10-16 12:02:25 +0100

    save omar zaoujal

---
+++ Programmation structurée/Partie 2- Langage C/Groupe A/COMPOsé/Omar Zaoujal/tableau composé/3-Déclaration caractére/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+		char T[5]={'h','e','l','l','o'}, a=0;
+	T[7]=a;
+	for(a=0; a<5 ; a++){
+		printf("%c", T[a]);
+	
+	}
+	return 0;
+}
---
+++ Programmation structurée/Partie 2- Langage C/Groupe E/Boucle/Chaimae Mrabet/Exemples composé - Boucles/Boucle avec Break/main7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main( ) { 
+int i;
+ for (i = 1 ; i<=10 ; i++){ 
+  printf("%d\n",i); 
+ if (i==4) 
+ break ; 
+ } 
+
+}
