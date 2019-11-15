8237d8b81d867b011f052a9b0931182bcf435d25
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-11-15 12:42:55 +0100

    Kebyani

---
+++ Programmation structurée/S4 - Structure/P1-Structure/Groupe C/Mouad Kebyani/2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ typedef struct stagaire {
+char Nom [100];
+int id [100]
+ };
+ stagaire;
+int main(int argc, char *argv[]) {
+		struct stagaire s1;
+	printf("Donner votre Nom: ");
+	scanf("%s",s1.Nom);
+	printf("Donner votre id: ");
+	scanf("%s",s1.id);
+	return 0;
+}
