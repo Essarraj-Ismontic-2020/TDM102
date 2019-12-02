9c1184d6de99b49934ff76370ad99328cc8dca81
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-02 09:37:36 +0100

    save yassine

---
+++ POO/2.Création d'un objet/P5.ManyToOne/Yassine El atlassi/Programs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut c#");
+            stagaire yassine = new stagaire();
+            yassine.nom = "yassine el atlassi";
+            yassine.age = 18;
+            yassine.id = "A48456";
+        }
+    }
+    public class stagaire
+    {
+        public string nom;
+        public int age;
+        public string id;
+    }
+}
