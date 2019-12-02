fdc0169809b69b21df765a0e00942efdf5161640
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-02 09:27:11 +0100

    kebyani

---
+++ POO/Exemple bonjour c#/omar zaoujal/omar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace omaroi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bonjour !");
+        }
+    }
+}
