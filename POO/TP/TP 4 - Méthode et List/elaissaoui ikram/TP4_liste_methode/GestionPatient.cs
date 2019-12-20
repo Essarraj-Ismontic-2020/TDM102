37f0154e6ca356aef221c3b0bd48e5471d5e2075
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-20 12:17:29 +0100

    save

---
+++ POO/TP/TP 4 - Méthode et List/elaissaoui ikram/TP4_liste_methode/GestionPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP4_liste_methode
+{
+    class GestionPatient
+    {
+        private List<Patient> _ListePatient;
+        private long compteur;
+        public GestionPatient()
+        {
+            this._ListePatient = new List<Patient>();
+            compteur = 0;
+        }
+        public void Ajouter (Patient p)
+        {
+            compteur = compteur + 1;
+            p.Id = compteur; //++compteur
+            this._ListePatient.Add(p);
+        }
+    }
+}
---
+++ POO/TP/TP 4 - Méthode et List/elaissaoui ikram/TP4_liste_methode/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP4_liste_methode
+{
+    class Patient
+    {
+        public long Id { get; set; }
+        public string Nom { get; set; }
+        public DateTime DateNaissance { get; set; }
+    }
+}
