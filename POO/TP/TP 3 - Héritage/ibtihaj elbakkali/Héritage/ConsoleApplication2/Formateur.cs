f1e1fc321108c7453123a386e0e18eb3fa41a6cc
Stagiaire-Ismontic <tp.stagiaire.ismontic@gmail.com>
2019-12-25 13:00:32 +0100

    save

---
+++ POO/TP/TP 3 - Héritage/ibtihaj elbakkali/Héritage/ConsoleApplication2/Formateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Formateur : personne
+    {
+        public Formateur(string Nom):base(Nom) { }
+        public override string ToString()
+        {
+            return $"Formateur { this.Nom } { this.DateNaisance}";
+        }
+        public override bool Equals(object obj)
+        {
+            personne personne = obj as personne;
+            if (this.Nom == personne.Nom) return true;
+            else { return false; }
+        }
+    }
+}
